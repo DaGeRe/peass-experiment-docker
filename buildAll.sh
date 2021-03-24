55008830b2aa448b5cb17399cb29cb5d29f614e4
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-03-24 22:23:36 +0100

    Change to correct project

--- buildAll.sh
+++ buildAll.sh
@@ -1,4 +1,4 @@
 cd peass && mvn clean install -DskipTests && cd ..
 cd precision-experiments/precision-analysis && ../gradlew clean publishToMavenLocal && cd ../../
-cd precision-experiments/precision-analysis && mvn clean install && cd ../../
+cd pmd-check/analysis && mvn clean install && cd ../../
 cd precision-experiments-rca && mvn clean install && cd ../
